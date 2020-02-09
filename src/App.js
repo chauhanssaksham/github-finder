81fbd160a9c9a7025fad897d1d6643e9ba2c61b1
Saksham <chauhanssaksham@gmail.com>
2020-02-10 01:20:54 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 import { render } from '@testing-library/react';
 
@@ -6,7 +8,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar/>
       </div>
     );
   }
