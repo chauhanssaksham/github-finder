04e21fd984811aa79920b3f441fec15bf9e1b93c
Saksham <chauhanssaksham@gmail.com>
2020-02-10 02:47:26 +0530

    Added UserItem & Users components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 import { render } from '@testing-library/react';
 
@@ -9,6 +9,9 @@ class App extends Component{
     return (
       <div className="App">
         <Navbar/>
+        <div className="container">
+          <Users/>
+        </div>
       </div>
     );
   }
