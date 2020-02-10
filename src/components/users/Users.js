780623ac830993d9b39a85589520726de1edbfca
Saksham <chauhanssaksham@gmail.com>
2020-02-10 18:27:40 +0530

    Added a Spinner for loading

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-    state = {
-        users: [
-            {
-                login : "mojombo",
-                id: 1,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo"
-            }, 
-            {
-                login : "Yolo2",
-                id: 2,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo"
-            },
-            {
-                login : "mojombo3",
-                id: 3,
-                avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-                html_url: "https://github.com/mojombo"
-            }
-        ]
-    };
-    render() {
+
+const Users = ({users, loading}) =>  {
+    if(loading){
+        return <Spinner/>
+    }
+    else{
         return (
             <div style={userStyle}>
-                {this.props.users.map(user => (
+                {users.map(user => (
                     <UserItem key={user.id} user={user} />
             ))}
             </div> 
-        )
-    };
+        );
+    }
 }
 const userStyle = {
     display: 'grid',
     gridTemplateColumns : 'repeat(3, 1fr)',
     gridGap: '1rem'
+}
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired,
 }
 
 export default Users
